c8eac85f7ebd1475172d52848fc54470c3535a0f
Nik Osvalds <nosvalds@gmail.com>
2020-06-09 14:34:15 +0100

    add logo and favicon

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+
+const Header = () => (
+    <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+        <img
+            alt="gratitude logo"
+            src="http://www.clker.com/cliparts/c/1/3/c/13601006691734787338gratitude-th.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+        />{' '}
+            Gratefulness Journal
+        </Navbar.Brand>
+    </Navbar>
+)
+
+export default Header;
